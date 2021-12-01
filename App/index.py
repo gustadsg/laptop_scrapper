bf452f672dba0438565853c93a6f84b7318dd397
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-12-01 15:09:40 -0300

    feat: recieve and apply query params

--- App/index.py
+++ App/index.py
@@ -1,12 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from scrapper.Scrapper import Scrapper
 
 app = Flask(__name__)
 
 @app.route('/')
 def scrap():
+  q = request.args.get('q')
+  min_price = request.args.get('min_price')
+  max_price = request.args.get('max_price')
+  reverse = request.args.get('reverse')
+  
   scrapper_instance = Scrapper()
-  result = scrapper_instance.scrap()
+  result = scrapper_instance.scrap(q, min_price, max_price, reverse)
   return jsonify(result)
 
 if __name__ == '__main__':
--- App/scrapper/Scrapper.py
+++ App/scrapper/Scrapper.py
@@ -16,24 +16,24 @@ class Scrapper:
     self.driver = webdriver.Firefox(options=options)
     self.driver.get(WEBSITE_URL)
   
-  def scrap(self, q=False, minPrice=False, maxPrice=False, reverse=False):
+  def scrap(self, q=False, min_price=False, max_price=False, reverse=False):
     self.products = self.get_all_products()
-    self.apply_filters( q, minPrice, maxPrice)
+    self.apply_filters( q, min_price, max_price)
 
     self.sort_products_by_price(self.products, reverse=reverse)
     return self.products
 
-  def apply_filters(self, q, minPrice, maxPrice):
+  def apply_filters(self, q, min_price, max_price):
     if q:
       filtered_by_description = self.filter_by_description(q)
       filtered_by_title = self.filter_by_title(q)
       filtered = filtered_by_description + filtered_by_title
       self.products = filtered
     
-    if minPrice:
-      self.products = self.filter_by_min_price(minPrice)
-    if maxPrice:
-      self.products = self.filter_by_min_price(minPrice)
+    if min_price:
+      self.products = self.filter_by_min_price(min_price)
+    if max_price:
+      self.products = self.filter_by_min_price(min_price)
 
   def filter_by_description(self, description):
     return [product for product in self.products if description.lower() in product["description"].lower()]
@@ -41,8 +41,8 @@ class Scrapper:
   def filter_by_title(self, title):
     return [product for product in self.products if title in product["title"]]
   
-  def filter_by_min_price(self, minPrice):
-    return [product for product in self.products if product["price"] >= minPrice]
+  def filter_by_min_price(self, min_price):
+    return [product for product in self.products if product["price"] >= min_price]
 
   def get_all_products(self):
     container = self.driver.find_element(By.CSS_SELECTOR ,"div.col-md-9 > div.row")

ec9e5f3ece9b18534a5acdb405c3a9735e9f5853
gustadsg@outlook.com.br <gustadsg@outlook.com.br>
2021-12-01 14:37:42 -0300

    feat: create get products route

---
+++ App/index.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from scrapper.Scrapper import Scrapper
+
+app = Flask(__name__)
+
+@app.route('/')
+def scrap():
+  scrapper_instance = Scrapper()
+  result = scrapper_instance.scrap("lenovo")
+  return jsonify(result)
+
+if __name__ == '__main__':
+  app.run()
